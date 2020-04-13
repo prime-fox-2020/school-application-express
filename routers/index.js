d72187df2c3ca4806a29be94b71b6b52720a29f7
permaficus <abukhalif2019@gmail.com>
2020-04-13 18:50:48 +0700

    Adding dynamic endpoint

--- routers/index.js
+++ routers/index.js
@@ -1,9 +1,16 @@
 const routes = require('express').Router();
 const {Home, Teachers, Students, Subjects} = require('../controllers')
 
-routes.get('/', Home.send)
-routes.get('/teachers', Teachers.send)
-routes.get('/students', Students.send)
-routes.get('/subjects', Subjects.send)
+routes.get('/', Home.render)
+routes.get('/teachers/:id', Teachers.render)
+routes.get('/teachers', Teachers.render)
+routes.get('/students/:email', Students.render)
+routes.get('/students', Students.render)
+routes.get('/subjects/:id', Subjects.render)
+routes.get('/subjects', Subjects.render)
+
+routes.get('*', (request, response) => {
+    response.status(404).render('404')
+})
 
 module.exports = routes;
