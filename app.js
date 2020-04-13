2fc55795ee3f5f24f0694f8284c27dc73fdb66a4
unknown <sonymartha4@gmail.com>
2020-04-13 23:19:56 +0700

    edit done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
 
 const router = require('./routes/router')
 app.use(router)
--- routes/students.js
+++ routes/students.js
@@ -2,7 +2,6 @@ const { Router } = require('express')
 const router = Router()
 
 const fs = require('fs')
-console.log(process.cwd());
 
 // fs.readFile('../students.json', 'utf8', (err, data) => {
 //     if (err) {
@@ -20,7 +19,89 @@ router.get('/students', function (req, res) {
         } else {
             let students_list = JSON.parse(data)
             // res.send(students_list)
-            res.render('students', {students_list})
+            res.render('students', { students_list })
+        }
+    })
+})
+
+
+router.get('/students/add', (req, res) => {
+    res.render('addStudent')
+})
+
+router.post('/students/add', (req, res) => {
+    const first_name = req.body.first_name
+    const last_name = req.body.last_name
+    const email = req.body.email
+    const gender = req.body.gender
+    const birth_date = req.body.birth_date
+
+    //read
+    fs.readFile('./students.json', 'utf8', (err, data) => {
+        if (err) {
+            res.send(err)
+        } else {
+            let students_list = JSON.parse(data)
+            let temp = {
+                "id": students_list[students_list.length - 1].id + 1,
+                "first_name": first_name,
+                "last_name": last_name,
+                "email": email,
+                "gender": gender,
+                "birth_date": birth_date
+            }
+            students_list.push(temp)
+            fs.writeFileSync(`./students.json`,JSON.stringify(students_list,null,2), 'utf8')
+                    res.render('students', { students_list })
+        }
+    })
+})
+
+router.get('/students/:id?/edit', (req, res) => {
+    //read
+    fs.readFile('./students.json', 'utf8', (err, data) => {
+        if (err) {
+            res.send(err)
+        } else {
+            let students_list = JSON.parse(data)
+            let length = students_list.length
+            for (let i = 0; i < length + 1; i++) {
+                if (req.params.id == students_list[i].id) {
+                    let dataById = students_list[req.params.id - 1]
+                    res.render('editStudent', { dataById })
+                    break
+                }
+            }
+        }
+    })
+})
+
+router.post('/students/:id?/edit', (req, res) => {
+    fs.readFile('./students.json', 'utf8', (err, data) => {
+        if (err) {
+            res.send(err)
+        } else {
+            let students_list = JSON.parse(data)
+            
+            const id = req.params.id
+            const first_name = req.body.first_name
+            const last_name = req.body.last_name
+            const email = req.body.email
+            const gender = req.body.gender
+            const birth_date = req.body.birth_date
+
+            let temp = {
+                "id": req.params.id,
+                "first_name": first_name,
+                "last_name": last_name,
+                "email": email,
+                "gender": gender,
+                "birth_date": birth_date
+            }
+
+            students_list[req.params.id-1] = temp
+            fs.writeFileSync(`./students.json`,JSON.stringify(students_list,null,2), 'utf8')
+            res.render('students', { students_list })
         }
     })
 })
