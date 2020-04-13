f596d799c3987ccc634b170163a67c1518547247
permaficus <abukhalif2019@gmail.com>
2020-04-13 17:17:51 +0700

    Using EJS & Body parser

--- servers/index.js
+++ servers/index.js
@@ -2,16 +2,16 @@
 
 const express = require('express');
 const server = new express();
-// const parser = require('body-parser');
+const parser = require('body-parser');
 const routes = require('../routers')
 
 class WebServer {
     static LoadServer() {
         console.log(__dirname)
-/*         server.use(parser.urlencoded({extended: true}));
+        server.use(parser.urlencoded({extended: true}));
         server.use(parser.json());
-        server.set('view engine', 'ejs'); */
-        // server.use('/assets', express.static('public/assets')) --> this is works
+        server.set('view engine', 'ejs');
+        server.use('/assets', express.static('public/assets'))
         server.use('/', routes)
         server.listen(3000, () => console.log('Started on port 3000'))
     }
