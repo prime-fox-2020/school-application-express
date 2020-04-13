f97fd50235333d0a7dd310cb33ed468aa718e148
pippop21 <afifar38@gmail.com>
2020-04-13 21:52:04 +0700

    release 2 + add gender and birth date validity

--- app.js
+++ app.js
@@ -1,49 +1,154 @@
-const fs      = require('fs');
-const express = require('express');
-const app     = express();
-const port    = 3000 || process.env.PORT;
+const fs          = require('fs');
+const dateConvert = require('./class/dateConvert');
+const bodyParser  = require('body-parser');
+const express     = require('express');
+const app         = express();
+const port        = 3000 || process.env.PORT;
 
-//json file
-const teachers = fs.readFileSync('./db/teachers.json', 'utf8');
-const students = fs.readFileSync('./db/students.json', 'utf8');
-const subjects = fs.readFileSync('./db/subjects.json', 'utf8');
 
 //SetUp 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extends: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 app.get('/teachers', (req,res) => {
-  const teacher = JSON.parse(teachers);
-  res.render('teachers', {teacher : teacher});
+  fs.readFile('./db/teachers.json', 'utf8', (err, data) => {
+    if(err) throw err;
+    const teacher = JSON.parse(data);
+    res.render('teachers', {teacher : teacher});
+  });
 });
 
 app.get('/teachers/:id', (req,res) => {
-  const teacher = JSON.parse(teachers);
-  res.render('teachers', {teacher : [teacher[req.params.id-1]]});
+  fs.readFile('./db/teachers.json', 'utf8', (err, data) => {
+    if(err) throw err;
+    const teacher = JSON.parse(data);
+    res.render('teachers', {teacher : [teacher[req.params.id-1]]});
+  });
 });
 
 app.get('/students', (req,res) => {
-  const student = JSON.parse(students);
-  res.render('students', {student : student});
+  fs.readFile('./db/students.json', 'utf8', (err, data) => {
+    if(err) throw err;
+    const student = JSON.parse(data);
+    res.render('students', {student : student});
+  });
+});
+
+app.post('/students', (req,res) => {
+  fs.readFile('./db/students.json', 'utf8', (err, data) => {
+    if(err) console.log(err);
+    const student     = JSON.parse(data);
+    const first_name  = req.body.first_name;
+    const last_name   = req.body.last_name;
+    const email       = req.body.email;
+    const gender      = req.body.gender;
+    const birthday    = req.body.birthday;
+    const dateBirth   = dateConvert.dateToString(birthday);
+    
+    student.push({
+      id : student.length + 1,
+      first_name : first_name,
+      last_name : last_name,
+      email : email,
+      gender : gender,
+      birth_date: dateBirth
+    });
+    const stud = JSON.stringify(student, null, 2);
+    fs.writeFile('./db/students.json', stud, (err)=>{
+      if(err) console.log(err);
+      res.redirect('/students');
+    });
+  });
+});
+
+app.get('/students/add', (req,res) => {
+  res.render('students/add');
+});
+
+app.get('/students/:id/edit', (req,res) => {
+  fs.readFile('./db/students.json', 'utf8', (err,data) => {
+    if(err) throw err;
+    const student = JSON.parse(data);
+    const convert = dateConvert.stringToDate(student[req.params.id-1].birth_date);
+    student[req.params.id-1].birth_date = convert;
+    res.render('students/edit', {student : student[req.params.id-1]});
+  });
+});
+
+app.post('/students/:id', (req,res) => {
+  fs.readFile('./db/students.json', 'utf8', (err, data) => {
+    if(err) console.log(err);
+    const student     = JSON.parse(data);
+    const first_name  = req.body.first_name;
+    const last_name   = req.body.last_name;
+    const email       = req.body.email;
+    const gender      = req.body.gender;
+    const dateBirth   = dateConvert.dateToString(birthday);
+    const studen      = {
+      id : Number(req.params.id),
+      first_name : first_name,
+      last_name : last_name,
+      email : email,
+      gender : gender,
+      birth_date: dateBirth
+    };
+    student.splice(req.params.id-1, 1, studen);
+    const stud = JSON.stringify(student, null, 2);
+    fs.writeFile('./db/students.json', stud, (err)=>{
+      if(err) console.log(err);
+      res.redirect('/students');
+    });
+  });
+});
+
+app.get('/students/:id/delete', (req,res) => {
+  fs.readFile('./db/students.json', 'utf8', (err, data) => {
+    if(err) console.log(err);
+    const student     = JSON.parse(data);
+    for(let i = 0; i < student.length; i++){
+      if(Number(req.params.id) === student[i].id) student.splice(i, 1);
+    }
+    const stud = JSON.stringify(student, null, 2);
+    fs.writeFile('./db/students.json', stud, (err)=>{
+      if(err) console.log(err);
+      res.redirect('/students');
+    });
+  });
 });
 
 app.get('/students/:email', (req,res) => {
-  const student = JSON.parse(students);
-  res.render('students', {student : [student[req.params.email-1]]});
+  fs.readFile('./db/students.json', 'utf8', (err, data) => {
+    if(err) throw err;
+    const student = JSON.parse(data);
+    let index;
+    for(let i = 0; i < student.length; i++){
+      if(req.params.email === student[i].email) index = i;
+    }
+    res.render('students', {student : [student[index]]});
+  });
 });
 
 app.get('/subjects', (req,res) => {
-  const subject = JSON.parse(subjects);
-  res.render('subjects', {subject : subject});
+  fs.readFile('./db/subjects.json', 'utf8', (err, data) => {
+    if(err) throw err;
+    const subject = JSON.parse(data);
+    res.render('subjects', {subject : subject});
+  });
 });
 
 app.get('/subjects/:id', (req,res) => {
-  const subject = JSON.parse(subjects);
-  res.render('subjects', {subject : [subject[req.params.id-1]]});
+  fs.readFile('./db/subjects.json', 'utf8', (err, data) => {
+    if(err) throw err;
+    const subject = JSON.parse(data);
+    
+    res.render('subjects', {subject : [subject[req.params.id-1]]});
+  });
 });
 
 app.listen(port, () => console.log('School app is started'));
