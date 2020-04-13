11b546ecda4196e88708c9227950086ee19a91ec
vinita-dewi <61119004+vinita-dewi@users.noreply.github.com>
2020-04-13 23:53:07 +0700

    Delete home.js

--- routes/home.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-const express = require('express')
-const routes = express.Router()
-
-routes.get('/', (req, res) => {
-    res.render('home')
-})
-
-module.exports = routes
