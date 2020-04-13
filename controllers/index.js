c3beef1af251a45da418bd4d5c076d596f3fcaa5
permaficus <abukhalif2019@gmail.com>
2020-04-13 23:27:27 +0700

    Updating CRUD function

--- controllers/index.js
+++ controllers/index.js
@@ -4,24 +4,69 @@ class Render {
     
     static page = (page, request, response) => {
 
-        console.log(request.params)
-
+        if (request.params.id == 'add' || request.params.email == 'add') {
+            response.render('new', {form: page, data: null})
+            return;
+        }
+        
         switch (true) {
             case request.params.hasOwnProperty('id') : {
                 Models.findById(page, 'id', request.params.id)
-                    .then(result => response.render(page, {data: result}))
+                    .then(result => {
+                        if (!request.params.hasOwnProperty('action')) {
+                            response.render(page, {data: result, readyForAction: true})
+                        } else {
+                            
+                            switch (request.params.action) {
+                                case 'edit' : {
+                                    Models.findById(page, 'id', request.params.id)
+                                        .then(result => {
+                                            response.render('new', {form: page, data: result})
+                                        })
+                                }; break;
+
+                                case 'delete' : {
+                                    Models.delete(page, request.params.id)
+                                        .then(redir => response.redirect(302, `/${redir}`))
+                                        .catch(err => response.send(err))
+                                }
+                            }
+
+                        }
+                    })
                     .catch(err => response.send(err))
             }; break;
-            
+
             case request.params.hasOwnProperty('email') : {
                 Models.findById(page, 'email', request.params.email)
-                    .then(result => response.render(page, {data: result}))
+                    .then(result => {
+
+                        if (!request.params.hasOwnProperty('action')) {
+                            response.render(page, {data: result, readyForAction: true})
+                        } else {
+
+                            switch (request.params.action) {
+                                case 'edit' : {
+                                    Models.findById(page, 'email', request.params.email)
+                                        .then(result => {
+                                            response.render('new', {form: page, data: result})
+                                        })
+                                }; break;
+
+                                case 'delete' : {
+                                    Models.delete(page, request.params.email)
+                                        .then(redir => response.redirect(302, `/${redir}`))
+                                        .catch(err => response.send(err))
+                                }; break;
+                            }
+                        }
+                    })
                     .catch(err => response.send(err))
-            }
+            }; break;
 
             default : {
                 Models.db(page)
-                    .then(result => response.render(page, {data: result}))
+                    .then(result => response.render(page, {data: result, readyForAction: false}))
                     .catch(err => response.send(err))
             }
         }
@@ -40,8 +85,18 @@ class Home {
 class Teachers {
 
     static render = (request, response) => {
-        
+
         Render.page('teachers', request, response)
+    }
+
+    static post = (request, response) => {
+        if (!request.params.hasOwnProperty('id')) {
+            Models.newData('teachers', request.body)
+        } else {
+            Models.update('teachers', request.body)
+        }
+
+        response.status(302).redirect('/teachers')
     }
 
 }
@@ -54,12 +109,28 @@ class Students {
 
     }
 
+    static post = (request, response) => {
+
+        if (!request.params.hasOwnProperty('id')) {
+            Models.newData('students', request.body)
+        } else {
+            Models.update('students', request.body)
+        }
+
+        response.redirect(302, '/students')
+    }
+
 }
 
 class Subjects {
 
     static render = (request, response) => {
         Render.page('subjects', request, response)
+    }
+
+    static post = (request, response) => {
+        Models.newData('subjects', request.body)
+        response.redirect(302, '/subjects')
     }
 
 }
